79f93d07a813d998834cd3882fa56ccb407f85c0
Giancarlo Martini <giancarlomartini@gmail.com>
2022-10-07 18:34:18 +0200

    Modificato il readme

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
-/*      main.c
-        semplice esempio di gestione dei parametri da linea di comando
+/***************************************************************************
+	file				 : main.c
+    begin                : 2019
+    copyright            : (C) 2019 by Giancarlo Martini and friends
+    email                : gm@giancarlomartini.it
 
-        argn = numero degli argomenti
-        argv = puntatore a un vettore di stringhe
-        Compilare con gcc main.c -o arg
-        Eseguire con ./arg xxx yyy
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
 */
-
 #include <stdio.h>
 
 int main(int argc, char **argv /* Oppure char *argv[] */)
